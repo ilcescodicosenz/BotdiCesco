86933f95f0e5f0fe8ee5477f333697f9f24684c2
ilcescodicosenz <cescokek@gmail.com>
2024-08-25 15:09:31 +0200

    Update cloudDBAdapter.js

--- cloudDBAdapter.js
+++ cloudDBAdapter.js
@@ -1,5 +1,6 @@
 import got from 'got';
 import { Buffer } from 'buffer';
+
 const stringify = (obj) => JSON.stringify(obj, null, 2);
 const parse = (str) => JSON.parse(str, (_, v) => {
   if (v !== null && typeof v === 'object' && 'type' in v && v.type === 'Buffer' && 'data' in v && Array.isArray(v.data)) {
@@ -7,19 +8,8 @@ const parse = (str) => JSON.parse(str, (_, v) => {
   }
   return v;
 });
-/**
- * CloudDBAdapter
- * @class
- */
+
 class CloudDBAdapter {
-  /**
-   * Costruttore
-   * @param {string} url - URL del database
-   * @param {object} [options] - Opzioni
-   * @param {function} [options.serialize] - Funzione di serializzazione
-   * @param {function} [options.deserialize] - Funzione di deserializzazione
-   * @param {object} [options.fetchOptions] - Opzioni per la richiesta HTTP
-   */
   constructor(url, {
     serialize = stringify,
     deserialize = parse,
@@ -31,11 +21,6 @@ class CloudDBAdapter {
     this.fetchOptions = fetchOptions;
   }
 
-  /**
-   * Legge il database
-   * @async
-   * @returns {Promise<object>} Oggetto deserializzato
-   */
   async read() {
     try {
       const res = await got(this.url, {
@@ -45,28 +30,70 @@ class CloudDBAdapter {
         },
         ...this.fetchOptions,
       });
-      if (res.statusCode !== 200) throw res.statusMessage;
+      if (res.statusCode !== 200) throw new Error(res.statusMessage);
       return this.deserialize(res.body);
     } catch (e) {
+      console.error('Errore di lettura:', e);
       return null;
     }
   }
-  /**
-   * Scrive sul database
-   * @async
-   * @param {object} obj - Oggetto da serializzare
-   * @returns {Promise<string>} Risposta del server
-   */
+
   async write(obj) {
-    const res = await got(this.url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      ...this.fetchOptions,
-      body: this.serialize(obj),
-    });
-    if (res.statusCode !== 200) throw res.statusMessage;
-    return res.body;
+    try {
+      const res = await got(this.url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        ...this.fetchOptions,
+        body: this.serialize(obj),
+      });
+      if (res.statusCode !== 200) throw new Error(res.statusMessage);
+      return res.body;
+    } catch (e) {
+      console.error('Errore di scrittura:', e);
+      return null;
+    }
+  }
+
+  async update(id, updates) {
+    try {
+      const res = await got(`${this.url}/${id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        ...this.fetchOptions,
+        body: this.serialize(updates),
+      });
+      if (res.statusCode !== 200) throw new Error(res.statusMessage);
+      return res.body;
+    } catch (e) {
+      console.error('Errore di aggiornamento:', e);
+      return null;
+    }
+  }
+
+  async delete(id) {
+    try {
+      const res = await got(`${this.url}/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        ...this.fetchOptions,
+      });
+      if (res.statusCode !== 200) throw new Error(res.statusMessage);
+      return res.body;
+    } catch (e) {
+      console.error('Errore di eliminazione:', e);
+      return null;
+    }
   }
 }
+
+const db = new CloudDBAdapter('https://example.com/db');
+db.read().then(data => console.log(data));
+db.write({ key: 'value' }).then(response => console.log(response));
+db.update('123', { key: 'newValue' }).then(response => console.log(response));
+db.delete('123').then(response => console.log(response));
