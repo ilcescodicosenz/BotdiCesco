e2bd36607a565c79a89c26dbda959959c8cb3d5f
ilcescodicosenz <cescokek@gmail.com>
2024-08-25 14:54:54 +0200

    Update database.js

--- database.js
+++ database.js
@@ -1,69 +1,91 @@
-import {resolve, dirname as _dirname} from 'path'; 
- import _fs, {existsSync, readFileSync} from 'fs'; 
- const {promises: fs} = _fs; 
-  
- class Database { 
-   /** 
-      * Create new Database 
-      * @param {String} filepath Path to specified json database 
-      * @param  {...any} args JSON.stringify arguments 
-      */ 
-   constructor(filepath, ...args) { 
-     this.file = resolve(filepath); 
-     this.logger = console; 
-  
-     this._load(); 
-  
-     this._jsonargs = args; 
-     this._state = false; 
-     this._queue = []; 
-     this._interval = setInterval(async () => { 
-       if (!this._state && this._queue && this._queue[0]) { 
-         this._state = true; 
-         await this[this._queue.shift()]().catch(this.logger.error); 
-         this._state = false; 
-       } 
-     }, 1000); 
-   } 
-  
-   get data() { 
-     return this._data; 
-   } 
-  
-   set data(value) { 
-     this._data = value; 
-     this.save(); 
-   } 
-  
-   /** 
-      * Queue Load 
-      */ 
-   load() { 
-     this._queue.push('_load'); 
-   } 
-  
-   /** 
-      * Queue Save 
-      */ 
-   save() { 
-     this._queue.push('_save'); 
-   } 
-  
-   _load() { 
-     try { 
-       return this._data = existsSync(this.file) ? JSON.parse(readFileSync(this.file)) : {}; 
-     } catch (e) { 
-       this.logger.error(e); 
-       return this._data = {}; 
-     } 
-   } 
-  
-   async _save() { 
-     const dirname = _dirname(this.file); 
-     if (!existsSync(dirname)) await fs.mkdir(dirname, {recursive: true}); 
-     await fs.writeFile(this.file, JSON.stringify(this._data, ...this._jsonargs)); 
-     return this.file; 
-   } 
- } 
-  
- export default Database;
+import { resolve, dirname as _dirname } from 'path';
+import _fs, { existsSync, readFileSync } from 'fs';
+const { promises: fs } = _fs;
+
+class Database {
+  /**
+   * Create new Database
+   * @param {String} filepath Path to specified json database
+   * @param  {...any} args JSON.stringify arguments
+   */
+  constructor(filepath, ...args) {
+    this.file = resolve(filepath);
+    this.logger = console;
+
+    this._load();
+
+    this._jsonargs = args;
+    this._state = false;
+    this._queue = [];
+    this._interval = setInterval(async () => {
+      if (!this._state && this._queue && this._queue[0]) {
+        this._state = true;
+        await this[this._queue.shift()]().catch(this.logger.error);
+        this._state = false;
+      }
+    }, 1000);
+  }
+
+  get data() {
+    return this._data;
+  }
+
+  set data(value) {
+    this._data = value;
+    this.save();
+  }
+
+  /**
+   * Queue Load
+   */
+  load() {
+    this._queue.push('_load');
+  }
+
+  /**
+   * Queue Save
+   */
+  save() {
+    this._queue.push('_save');
+  }
+
+  _load() {
+    try {
+      return this._data = existsSync(this.file) ? JSON.parse(readFileSync(this.file)) : {};
+    } catch (e) {
+      this.logger.error(e);
+      return this._data = {};
+    }
+  }
+
+  async _save() {
+    const dirname = _dirname(this.file);
+    if (!existsSync(dirname)) await fs.mkdir(dirname, { recursive: true });
+    await fs.writeFile(this.file, JSON.stringify(this._data, ...this._jsonargs));
+    return this.file;
+  }
+
+  // Nuova funzione: Aggiungere una voce al database
+  add(key, value) {
+    this._data[key] = value;
+    this.save();
+  }
+
+  // Nuova funzione: Rimuovere una voce dal database
+  remove(key) {
+    delete this._data[key];
+    this.save();
+  }
+
+  // Nuova funzione: Trovare una voce nel database
+  find(key) {
+    return this._data[key] || null;
+  }
+
+  // Nuova funzione: Verifica se una chiave esiste
+  exists(key) {
+    return this._data.hasOwnProperty(key);
+  }
+}
+
+export default Database;
