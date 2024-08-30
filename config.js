91adb429ae9b4184946efb10271b5f753aa60e63
ilcescodicosenz <cescokek@gmail.com>
2024-08-30 18:04:40 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,28 +1,36 @@
-import { watchFile, unwatchFile } from 'fs'
-import chalk from 'chalk'
-import fs from 'fs'
-import { fileURLToPath } from 'url'
+import { watchFile, unwatchFile } from 'fs';
+import chalk from 'chalk';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
 
-global.botnumber = ""
-global.confirmCode = ""
+global.botnumber = "";
+global.confirmCode = "";
 
 global.owner = [
   ['+393293262584', 'Cesco', true],
-]
+];
 
-global.mods = ['xxxxxxxxxx'] 
-global.prems = ['xxxxxxxxxx', 'xxxxxxxxxx']
+global.mods = ['xxxxxxxxxx'];
+global.prems = ['xxxxxxxxxx', 'xxxxxxxxxx'];
 
-global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
-global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
-global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
-global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
-global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
-global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
-global.lolkeysapi = ['BrunoSobrino']
+global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124'];
+global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())];
+global.keysxteammm = [
+  '29d4b59a4aa687ca',
+  '5LTV57azwaid7dXfz5fzJu',
+  'cb15ed422c71a2fb',
+  '5bd33b276d41d6b4',
+  'HIRO',
+  'kurrxd09',
+  'ebb6251cc00f9c63',
+];
+global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())];
+global.keysneoxrrr = ['5VC9rvNx', 'cfALv5'];
+global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())];
+global.lolkeysapi = ['BrunoSobrino'];
 
-global.APIs = { 
-  xteam: 'https://api.xteam.xyz', 
+global.APIs = {
+  xteam: 'https://api.xteam.xyz',
   nrtm: 'https://fg-nrtm-nhie.onrender.com',
   bg: 'http://bochil.ddns.net',
   fgmods: 'https://api-fgmods.ddns.net',
@@ -32,30 +40,30 @@ global.APIs = {
   neoxr: 'https://api.neoxr.my.id',
   zenzapis: 'https://zenzapis.xyz',
   akuari: 'https://api.akuari.my.id',
-  akuari2: 'https://apimu.my.id',	
-}
+  akuari2: 'https://apimu.my.id',
+};
 
-global.APIKeys = { 
+global.APIKeys = {
   'https://api.xteam.xyz': `${keysxteam}`,
   'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
-  'https://api.neoxr.my.id': `${keysneoxr}`,	
+  'https://api.neoxr.my.id': `${keysneoxr}`,
   'https://violetics.pw': 'beta',
-}
+};
 
-global.imagen1 = ['./media/menu1.jpg']
-global.imagen4 = fs.readFileSync('./bixbyvision16.png')
+global.imagen1 = ['./media/menu1.jpg'];
+global.imagen4 = fs.readFileSync('./bixbyvision16.png');
 
-global.packname = 'BotdiCesco'
-global.author = 'Cesco'
+global.packname = 'BotdiCesco';
+global.author = 'Cesco';
 
-global.vs = '𝟐.𝟐'
-global.nomebot = 'BotdiCesco'
+global.vs = '𝟐.𝟐';
+global.nomebot = 'BotdiCesco';
 
-global.multiplier = 69 
-global.maxwarn = '4' 
+global.multiplier = 69;
+global.maxwarn = '4';
 
-global.wm = 'BotdiCesco'
-global.wait = 'ⓘ 𝐂𝐚𝐫𝐢𝐜𝐚𝐦𝐞𝐧𝐭𝐨 ...'
+global.wm = 'BotdiCesco';
+global.wait = 'ⓘ 𝐂𝐚𝐫𝐢𝐜𝐚𝐦𝐞𝐧𝐭𝐨 ...';
 
 global.flaaa = [
   'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&text=',
@@ -63,11 +71,11 @@ global.flaaa = [
   'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=',
   'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&text=',
   'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&text=',
-]
+];
 
-let file = fileURLToPath(import.meta.url)
+let file = fileURLToPath(import.meta.url);
 watchFile(file, () => {
-  unwatchFile(file)
-  console.log(chalk.redBright("Update 'config.js'"))
-  import(`${file}?update=${Date.now()}`)
-})
+  unwatchFile(file);
+  console.log(chalk.redBright("Update 'config.js'"));
+  import(`${file}?update=${Date.now()}`);
+});
